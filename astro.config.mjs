ca6782d13e6494f7acdb84f4bf05671af3491b13
Anurag Parida <anuragparida37@gmail.com>
2024-12-11 01:39:54 +0100

    basics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  site: process.env.CI ? 'https://anuragparida.com' : 'http://localhost:4321',
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+})
---
+++ src/lib/constants.ts
@@ -0,0 +1,92 @@
+import type { Site, Page } from './types'
+
+export const loaderAnimation = [
+  '.loader',
+  { opacity: [1, 0], pointerEvents: 'none' },
+  { easing: 'ease-out' },
+]
+
+export const LINKS = {
+  github: 'https://github.com/bue221',
+  linkedin: 'https://www.linkedin.com/in/bue221/',
+  mail: 'mailto:camiloplaza3@gmail.com',
+  instagram: 'https://www.instagram.com/caj_ink/',
+  medium: 'https://ladvace.medium.com/',
+  discord: 'https://discordapp.com/users/163300027618295808',
+}
+
+// Global
+export const SITE: Site = {
+  TITLE: 'Anurag Parida',
+  DESCRIPTION: 'Welcome to my personal corner of the internet!',
+  AUTHOR: 'Anurag Parida',
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: 'Work',
+  DESCRIPTION: 'Places or projects I have worked on.',
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: 'Blog',
+  DESCRIPTION: 'Writing on topics I am passionate about.',
+}
+
+// Projects Page
+export const PROJECTS: Page = {
+  TITLE: 'Projects',
+  DESCRIPTION: 'A couple of projects I have worked on.',
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: 'Search',
+  DESCRIPTION: 'Search all posts and projects by keyword.',
+}
+
+// Study Page
+export const STUDIES = [
+  {
+    title: 'Robotics',
+    institution: 'THWS',
+    link: 'https://www.ucentral.edu.co/',
+    date: '2022-2025',
+  },
+  {
+    title: '...',
+    institution: 'Platzi',
+    link: 'https://platzi.com/',
+    date: '2018 - 2022',
+  },
+]
+
+export const EXPERIENCE = [
+  {
+    company: 'Mercedes-Benz Group AG',
+    location: 'Sindelfingen, Germany',
+    position: 'Intern',
+    start: '2021',
+    link: 'https://straico.com/',
+    end: 'Current',
+    tasks: [
+      'Integration IA services with react and tailwind css',
+      'Development and build of DB with mongoDB',
+    ],
+  },
+  {
+    company: 'Spot2',
+    location: 'Mexico City, Mexico',
+    position: 'Software Engineer',
+    link: 'https://spot2.mx/',
+    start: '2021',
+    end: 'Current',
+    tasks: [
+      'Development of the Spot2 platform with the use of React, Redux, and Material UI',
+      'Development map with the use of Google Maps API',
+      'Development internal platform with the use of React, Redux, and Material UI',
+      'Testing and debugging',
+    ],
+  },
+]
